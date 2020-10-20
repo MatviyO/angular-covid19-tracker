3e86c9c261ec9af926c31e0e41d03042f6e1ea93
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-10-20 17:28:58 +0300

    fff

--- covid-tracker/src/app/components/countries/countries.component.ts
+++ covid-tracker/src/app/components/countries/countries.component.ts
@@ -8,6 +8,10 @@ import {GlobalDataSummary} from '../../models/global-data';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
   data: GlobalDataSummary[];
   countries: string[] = [];
 
@@ -19,6 +23,18 @@ export class CountriesComponent implements OnInit {
       this.data.forEach(cs => {
         this.countries.push(cs.country);
       });
+    });
+  }
+  updateValue(country: string): any {
+    this.data.forEach(cs => {
+      if (cs.country === country) {
+        this.totalActive = cs.active;
+        this.totalConfirmed = cs.confirmed;
+        this.totalDeaths = cs.deaths;
+        this.totalRecovered = cs.recovered;
+      } else {
+
+      }
     });
   }
 }
